4ea8c7bd1b97bfff920d97f460d85be0c5491891
chriskevinlee <112503262+chriskevinlee@users.noreply.github.com>
2024-11-18 21:10:14 +0000

    Update volume.sh

--- config/qtile/scripts/volume.sh
+++ config/qtile/scripts/volume.sh
@@ -14,5 +14,5 @@ elif [ "$1" = "down" ]; then
     dunstify -r 8763 "Volume Control" "$current_volume"
 fi
 
-current_volume=$(pactl list sinks | awk '/^\s*Volume:/ {print $5}')
+current_volume=$(pactl list sinks | grep -m 1 'Volume:' | awk -F'/' '{print $2}' | sed 's/[^0-9%]//g')
 echo " 󰕾 $current_volume"
