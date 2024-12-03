aeedf0b128ea2c0c4bc4cf978b55ed44482b067b
chriskevinlee <112503262+chriskevinlee@users.noreply.github.com>
2024-12-03 22:19:51 +0000

    Update autostart.sh

--- config/qtile/scripts/autostart.sh
+++ config/qtile/scripts/autostart.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 xrandr -s 1920x1080 &
 
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+if command -v /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 >/dev/null 2>&1; then
+        /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1
+elif command -v /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 >/dev/null 2>&1; then
+        /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
+fi
 
 flameshot &
 
