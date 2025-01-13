b080a27ec16920f6d8df1bf2356d928ec3a15f0a
chriskevinlee <112503262+chriskevinlee@users.noreply.github.com>
2025-01-13 22:52:20 +0000

    Update nmcli.sh

--- config/openbox/scripts/nmcli.sh
+++ config/openbox/scripts/nmcli.sh
@@ -13,16 +13,16 @@ connected_ssid=$(nmcli -t -f active,ssid dev wifi | grep '^yes' | cut -d':' -f2)
 # Determine current state and icon path
 if [[ $check_ethernet_wifi = "ethernet wifi" ]]; then
     current_state="ethernet_wifi"
-    icon_path="/home/chris/.config/openbox/png_icons/personal-computer.png"
+    icon_path="/home/$USER/.config/openbox/png_icons/personal-computer.png"
 elif [[ $check_wifi = "wifi" ]]; then
     current_state="wifi"
-    icon_path="/home/chris/.config/openbox/png_icons/wifi.png"
+    icon_path="/home/$USER/.config/openbox/png_icons/wifi.png"
 elif [[ $check_ethernet = "ethernet" ]]; then
     current_state="ethernet"
-    icon_path="/home/chris/.config/openbox/png_icons/computer.png"
+    icon_path="/home/$USER/.config/openbox/png_icons/computer.png"
 else
     current_state="disconnected"
-    icon_path="/home/chris/.config/openbox/png_icons/cancel.png"
+    icon_path="/home/$USER/.config/openbox/png_icons/cancel.png"
 fi
 
 # Check previous state
