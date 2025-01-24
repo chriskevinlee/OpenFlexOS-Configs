2a6657f244f91323239626ddce2849b51d8710e2
chriskevinlee <112503262+chriskevinlee@users.noreply.github.com>
2025-01-24 23:27:15 +0000

    Update volume.sh

--- config/qtile/scripts/volume.sh
+++ config/qtile/scripts/volume.sh
@@ -5,29 +5,35 @@ volume_decrease="-10%"
 volume_mute="toggle"
 notification_id=6534
 
-# Get current mute and volume status
-get_current_volume() {
-    #pactl list sinks | awk '/^\s*Volume:/ {print $5}' | head -n 1
-    pactl list sinks | grep -m 1 'Volume:' | awk '{print $5}'
+# Function to get the active sink
+get_active_sink() {
+    pactl info | grep 'Default Sink:' | awk '{print $3}'
 }
 
-get_mute_status() {
-    #pactl list sinks | awk '/Mute:/ {print $2}' | head -n 1
-    pactl list sinks | grep -m 1 'Mute:' | awk '{print $2}'
+# Get current volume of the active sink
+get_current_volume() {
+    pactl list sinks | grep -A 10 "$(get_active_sink)" | grep -m 1 'Volume:' | awk '{print $5}'
 }
+
+# Get mute status of the active sink
+get_mute_status() {
+    pactl list sinks | grep -A 10 "$(get_active_sink)" | grep 'Mute:' | awk '{print $2}'
+}
+
+active_sink=$(get_active_sink)
 
 if [ "$1" = "up" ]; then
-    pactl set-sink-volume @DEFAULT_SINK@ "$volume_increase"
+    pactl set-sink-volume "$active_sink" "$volume_increase"
     current_volume=$(get_current_volume)
     dunstify -r "$notification_id" "Volume Control" "$current_volume"
 
 elif [ "$1" = "down" ]; then
-    pactl set-sink-volume @DEFAULT_SINK@ "$volume_decrease"
+    pactl set-sink-volume "$active_sink" "$volume_decrease"
     current_volume=$(get_current_volume)
     dunstify -r "$notification_id" "Volume Control" "$current_volume"
 
 elif [ "$1" = "mute" ]; then
-    pactl set-sink-mute @DEFAULT_SINK@ "$volume_mute"
+    pactl set-sink-mute "$active_sink" "$volume_mute"
     mute_status=$(get_mute_status)
     if [ "$mute_status" = "yes" ]; then
         dunstify -r "$notification_id" "Volume Control" "Muted"
