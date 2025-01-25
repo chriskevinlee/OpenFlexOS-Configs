a7a36e0382080762740d2c7a86c8096dbca4b85e
chriskevinlee <112503262+chriskevinlee@users.noreply.github.com>
2025-01-25 12:54:35 +0000

    Update volume.sh

--- config/openbox/scripts/volume.sh
+++ config/openbox/scripts/volume.sh
@@ -5,41 +5,45 @@ volume_decrease="-10%"
 volume_mute="toggle"
 notification_id=6534
 
-# Function to get the active sink
-get_active_sink() {
-    pactl info | grep 'Default Sink:' | awk '{print $3}'
-}
-
-# Get current volume of the active sink
+# Get current mute and volume status
 get_current_volume() {
-    pactl list sinks | grep -A 10 "$(get_active_sink)" | grep -m 1 'Volume:' | awk '{print $5}'
+    pactl list sinks | grep -m 1 'Volume:' | awk '{print $5}'
 }
 
-# Get mute status of the active sink
 get_mute_status() {
-    pactl list sinks | grep -A 10 "$(get_active_sink)" | grep 'Mute:' | awk '{print $2}'
+    pactl list sinks | grep -m 1 'Mute:' | awk '{print $2}'
 }
-
-active_sink=$(get_active_sink)
 
 if [ "$1" = "up" ]; then
-    pactl set-sink-volume "$active_sink" "$volume_increase"
+    pactl set-sink-volume @DEFAULT_SINK@ "$volume_increase"
     current_volume=$(get_current_volume)
     dunstify -r "$notification_id" "Volume Control" "$current_volume"
 
 elif [ "$1" = "down" ]; then
-    pactl set-sink-volume "$active_sink" "$volume_decrease"
+    pactl set-sink-volume @DEFAULT_SINK@ "$volume_decrease"
     current_volume=$(get_current_volume)
     dunstify -r "$notification_id" "Volume Control" "$current_volume"
 
 elif [ "$1" = "mute" ]; then
-    pactl set-sink-mute "$active_sink" "$volume_mute"
+    pactl set-sink-mute @DEFAULT_SINK@ "$volume_mute"
     mute_status=$(get_mute_status)
     if [ "$mute_status" = "yes" ]; then
         dunstify -r "$notification_id" "Volume Control" "Muted"
     else
         dunstify -r "$notification_id" "Volume Control" "UnMuted"
     fi
+
+elif [ "$1" = "play" ]; then
+    playerctl play-pause
+    dunstify -r "$notification_id" "Media Control" "Play/Pause toggled"
+
+elif [ "$1" = "next" ]; then
+    playerctl next
+    dunstify -r "$notification_id" "Media Control" "Skipped to Next"
+
+elif [ "$1" = "prev" ]; then
+    playerctl previous
+    dunstify -r "$notification_id" "Media Control" "Went to Previous"
 
 elif [ -z "$1" ]; then
     mute_status=$(get_mute_status)
@@ -50,5 +54,5 @@ elif [ -z "$1" ]; then
         echo " $current_volume"
     fi
 else
-    echo "Usage: $0 {up|down|mute}"
+    echo "Usage: $0 {up|down|mute|play|next|prev}"
 fi
