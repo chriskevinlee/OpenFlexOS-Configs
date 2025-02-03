37527f538362443813906b955a6aaec2b934cf43
chriskevinlee <112503262+chriskevinlee@users.noreply.github.com>
2025-02-03 15:46:30 +0000

    Update config.py

--- config/qtile/config.py
+++ config/qtile/config.py
@@ -172,13 +172,14 @@ class VolumeWidget(TextBox):
     def on_middle_click(self):
         subprocess.run([get_script_path("volume.sh"), "mute"])
         self.update_volume()
-    # Create the widget instance globally so hooks can reference it
-    volume_widget = VolumeWidget()
 
-    # Qtile Hooks to Refresh the Widget
-    @hook.subscribe.startup
-    def update_volume_on_start():
-        volume_widget.update_volume()
+# Create the widget instance globally so hooks can reference it
+volume_widget = VolumeWidget()
+
+# Qtile Hooks to Refresh the Widget
+@hook.subscribe.startup
+def update_volume_on_start():
+    volume_widget.update_volume()
 
 # Function to display Screen brightnless and allow left click and right click
 class BrightnessWidget(TextBox):
@@ -254,21 +255,21 @@ reconfigure_screens = True
 keys = [
     # A list of available commands that can be bound to keys can be found
     # at https://docs.qtile.org/en/latest/manual/config/lazy.html
-
+    
     # Switch between windows
     Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
     Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
     Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
     Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
     Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
-
+    
     # Move windows between left/right columns or move up/down in current stack.
     # Moving out of range in Columns layout will create new column.
     Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
     Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
     Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
-
+    
     # Grow windows. If current window is on the edge of screen and direction
     # will be to screen edge - window would shrink.
     Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
@@ -276,17 +277,17 @@ keys = [
     Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
-
+    
     # Grow/resize Windows in monadtall
     Key([mod], "i", lazy.layout.grow()),
     Key([mod], "m", lazy.layout.shrink()),
-
+    
     # Grow/shrink/resize in floating mode
     Key([alt], "l", resize_floating_window(width=10), desc="Increase width by 10"),
     Key([alt], "h", resize_floating_window(width=-10), desc="Decrease width by 10"),
     Key([alt], "j", resize_floating_window(height=10), desc="Increase height by 10"),
     Key([alt], "k", resize_floating_window(height=-10), desc="Decrease height by 10"),
-
+    
     # Toggle between split and unsplit sides of stack.
     # Split = all windows displayed
     # Unsplit = 1 window displayed, like Max layout, but still with
@@ -295,27 +296,31 @@ keys = [
         [mod, "shift"], "Return", lazy.layout.toggle_split(),
         desc="Toggle between split and unsplit sides of stack",
     ),
-
+    
     Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
     Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
     Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
-
+    
     Key(
         [mod], "f", lazy.window.toggle_fullscreen(),
         desc="Toggle fullscreen on the focused window",
     ),
-
+    
     Key([mod], "t", lazy.window.toggle_floating(), desc="Toggle floating on the focused window"),
     Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
     Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
-
+    
+    # Start of My Config: setting my own keys
     Key([alt], "q", lazy.spawn(get_script_path("power.sh")), desc="Powermenu"),
     Key([alt], "d", lazy.spawn(get_script_path("applications.sh")), desc="Menu"),
     Key([alt], "f", lazy.spawn("firefox"), desc="Launch Firefox"),
     Key([], "XF86AudioRaiseVolume", lazy.spawn(get_script_path("volume.sh") + " up"), desc="Increase volume"),
     Key([], "XF86AudioLowerVolume", lazy.spawn(get_script_path("volume.sh") + " down"), desc="Decrease volume"),
-    Key([], "XF86AudioMute", lazy.spawn(get_script_path("volume.sh") + " mute"), desc="Mute/Unmute"),]
+    Key([], "XF86AudioMute", lazy.spawn(get_script_path("volume.sh") + " mute"), desc="Mute/Unmute"),
+
+    # End of My Config: setting my own keys
+]
 
 layouts = [
     layout.MonadTall(margin=15),
@@ -436,4 +441,5 @@ for i in groups:
             # # mod + shift + group number = move focused window to group
             # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
             #     desc="move focused window to group {}".format(i.name)),
-        
+        ]
+    )
