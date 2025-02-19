a79de0330c3464b195c30c96ec631f4ea626c44c
chriskevinlee <112503262+chriskevinlee@users.noreply.github.com>
2025-02-19 22:36:09 +0000

    Update nerd-dictation.sh

--- config/qtile/scripts/nerd-dictation.sh
+++ config/qtile/scripts/nerd-dictation.sh
@@ -6,17 +6,17 @@ STATE_FILE="$HOME/.config/nerd-dictation-state"
 if [[ -f "$STATE_FILE" ]]; then
     LAST_STATE=$(cat "$STATE_FILE")
 else
-    LAST_STATE="Right click to stop dictation"  # Default to "Right click to stop dictation"
+    LAST_STATE="Left-click: Dictate"  # Default to "Right-click: Stop"
 fi
 
 case $1 in
     start )
         nd begin &
-        echo "Right click to stop dictation" > "$STATE_FILE"  # Save state
+        echo "Right-click: Stop" > "$STATE_FILE"  # Save state
         ;;
     stop )
         nd end &
-        echo "Left click to start dictation" > "$STATE_FILE"  # Save state
+        echo "Left-click: Dictate" > "$STATE_FILE"  # Save state
         ;;
     * )
         # If no argument is given, print the last saved state
