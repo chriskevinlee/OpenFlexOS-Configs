aa56b19021027cb9562de696c01a15d4f295252a
chriskevinlee <112503262+chriskevinlee@users.noreply.github.com>
2025-03-03 15:14:49 +0000

    Update power.sh

--- config/qtile/scripts/power.sh
+++ config/qtile/scripts/power.sh
@@ -3,17 +3,6 @@
 #launcher="rofi -config /home/$USER/.config/qtile/rofi/config.rasi -dmenu"
 launcher="dmenu -i"
 
-active_sounds=yes
-sounds_dir="$HOME/.config/qtile/sounds/"
-
-login_sound="game-bonus-144751.mp3"
-lock_sound="ambient-piano-logo-165357.mp3"
-logout_sound="marimba-win-f-2-209688.mp3"
-reboot_sound="introduction-sound-201413.mp3"
-suspend_sound="cozy-weaves-soft-logo-176378.mp3"
-hibernate_sound="lovelyboot1-103697.mp3"
-poweroff_sound="retro-audio-logo-94648.mp3"
-
 # Use yes or no to enable or disable countdown
 enable_countdown=yes
 
