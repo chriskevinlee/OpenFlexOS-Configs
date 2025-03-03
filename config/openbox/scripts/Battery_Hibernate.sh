71207b2b4c40360f0a4744956b68f5b6a61e592a
chriskevinlee <112503262+chriskevinlee@users.noreply.github.com>
2025-03-03 14:50:31 +0000

    Update Battery_Hibernate.sh

--- config/openbox/scripts/Battery_Hibernate.sh
+++ config/openbox/scripts/Battery_Hibernate.sh
@@ -1,72 +1,88 @@
 #!/bin/bash
 
 # ================================================================
-# Description: This script will monitor device battery when it goes too low you will give a notifcation, once the battery is about to run out to gives a audible warning with a notifcation
+# Description: This script monitors the battery and notifies the user when it's low.
+# It also warns audibly and hibernates the system when the battery is critically low.
 # Author: Chris Lee, ChatGPT
-# Dependencies: mpv, dunstify
-# Usage: add to a startup script(Recommened) or ./Battery_Hibernate.sh from terminal(Not Recommened)
-# Notes: Feel free to ajust variables.
+# Dependencies: mpv, dunstify, inotify-tools
+# Notes: Sounds from https://pixabay.com
+# - https://pixabay.com/sound-effects/machine-error-by-prettysleepy-art-12669/
+# - https://pixabay.com/sound-effects/error-83494/
 # ================================================================
 
-# Set in Percentage when you get a notification when the battery is low
+### Warnning ###
+# Set battery percent to give a warning of low battery, Comment (#) to disable warning
 WARNING_PERCENT=31
+# Keep the nofication open for a set time in milliseconds
+WARNING_PERCENT_TIMEOUT=6000
+# Set the number of seconds until the nofications shows again after dispearing
+WARNING_PERCENT_NOTIFICATION=10
 
-# Set in Percentage when you get a notification and sound when the battery is low
+
+### Hibernate ###
+# Set battery percent to give a warning of the system is about to hibernate, Comment (#) to disable warning
 HIBERNATE_PERCENT=16
-
-# Set sound and notification time (in seconds)
-PRE_HIBERNATE=10
-
-# Set the countdown time before hibernation (in seconds)
+# Set Number of seconds that the "SOUND AND NOTIFICATIONS" will be displayed till the system hibernates
+HIBERNATE_PERCENT_NOTIFICATION=10
+# The number of seconds till the system hibernates. Default 120 seconds (2 minutes), when the nofication shows you will have 120 seconds till the syste hibernates
 HIBERNATE_WAIT=120
 
-while true; do
-    # Check if a battery is available
-    BATTERY_DIR=$(ls /sys/class/power_supply/ | grep BAT || echo "")
-    if [[ -z $BATTERY_DIR ]]; then
-        echo "No battery detected. Exiting."
-        exit 0
-    fi
+HIBERNATE_PERCENT_NOTIFICATION_SOUND=/home/$USER/.config/$DESKTOP_SESSION/sounds/error-83494.mp3
+HIBERNATE_PERCENT_TIMEOUT_SOUND=/home/$USER/.config/$DESKTOP_SESSION/sounds/machine-error-by-prettysleepy-art-12669.mp3
 
+
+# Get battery directory
+BATTERY_DIR=$(ls /sys/class/power_supply/ | grep BAT || echo "")
+if [[ -z $BATTERY_DIR ]]; then
+    echo "No battery detected. Exiting."
+    exit 0
+fi
+
+while true; do
     STATUS=$(cat /sys/class/power_supply/$BATTERY_DIR/status)
     CAPACITY=$(cat /sys/class/power_supply/$BATTERY_DIR/capacity)
 
-    # Skip checks if the charger is connected
+    # Detect wake-up from hibernation and force refresh
+    inotifywait -e modify /sys/class/power_supply/$BATTERY_DIR/capacity &>/dev/null
+    sleep 5  # Give system some time to update battery info
+
+    # Refresh values after wake-up
+    STATUS=$(cat /sys/class/power_supply/$BATTERY_DIR/status)
+    CAPACITY=$(cat /sys/class/power_supply/$BATTERY_DIR/capacity)
+
+    # Skip checks if charging
     if [[ $STATUS == "Charging" ]]; then
-        sleep 60  # Check again in 60 seconds
+        sleep 60
         continue
     fi
 
-    # Hibernation condition
-    if [[ $CAPACITY -lt $HIBERNATE_PERCENT ]] && [[ $STATUS != "Charging" ]]; then
-        # Countdown with periodic status checks
+    # Critical battery level - prepare to hibernate
+    if [[ $CAPACITY -lt $HIBERNATE_PERCENT ]]; then
         for ((i = $HIBERNATE_WAIT; i > 0; i--)); do
             STATUS=$(cat /sys/class/power_supply/$BATTERY_DIR/status)
             if [[ $STATUS == "Charging" ]]; then
                 break
             fi
-
-            # Play sound and send notification every 10 seconds
-            if (( i % $PRE_HIBERNATE == 0 )); then
-                mpv --no-video /home/chris/Downloads/error-83494.mp3 &
+            if (( i % HIBERNATE_PERCENT_NOTIFICATION == 0 )); then
+                mpv $HIBERNATE_PERCENT_NOTIFICATION_SOUND &
                 dunstify -u critical -r 55452 "Battery is at $CAPACITY%" \
                     "System about to Hibernate in $i seconds! Please Charge the Battery."
             fi
-
             sleep 1
         done
 
-        # If the loop wasn't interrupted by a charger connection, proceed to hibernate
+        # Hibernate if still low and not charging
         if [[ $STATUS != "Charging" ]]; then
-            mpv --no-video /home/chris/Downloads/machine-error-by-prettysleepy-art-12669.mp3
-            dunstify -u critical -r 55452 "Battery is at $CAPACITY%" "System is Hibernating Now!"
-            sleep 2
+            mpv $HIBERNATE_PERCENT_TIMEOUT_SOUND &
+            dunstify -t $HIBERNATE_PERCENT_TIMEOUT -u critical -r 55452 "Battery is at $CAPACITY%" "System is Hibernating Now!"
+            sleep 10
             systemctl hibernate
         fi
-    elif [[ $CAPACITY -lt $WARNING_PERCENT ]] && [[ $STATUS != "Charging" ]]; then
-        dunstify -r 55452 "Battery is at $CAPACITY%" "Please Charge the Battery"
-        sleep 10
+    elif [[ $CAPACITY -lt $WARNING_PERCENT ]]; then
+         dunstify -r 55452 "Battery is at $CAPACITY%" "Please Charge the Battery"
+         sleep $WARNING_PERCENT_NOTIFICATION
     fi
 
-    sleep 1  # Shorter delay for quicker responsiveness
+    sleep 1
 done
+
